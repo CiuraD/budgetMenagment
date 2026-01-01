46b494a825ea18e702ae19a6cb76f5f50c595308
ciurad <damianciura2001@gmail.com>
2026-01-01 13:40:16 +0100

    Initialization

---
+++ src/test/java/com/aghallegro/budgetmenagment/BudgetMenagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aghallegro.budgetmenagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetMenagmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
