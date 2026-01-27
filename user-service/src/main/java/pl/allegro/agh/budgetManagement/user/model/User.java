2259fef250f4783be9a38197f7d20d55a349bdb9
ciurad <damianciura2001@gmail.com>
2026-01-27 18:38:13 +0100

    added basic user entity

---
+++ user-service/src/main/java/pl/allegro/agh/budgetManagement/user/model/User.java
@@ -0,0 +1,25 @@
+package pl.allegro.agh.budgetManagement.user.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "\"user\"")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String nickname;
+}
