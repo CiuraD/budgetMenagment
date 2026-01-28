cd0a228742b3f351df55577afc56832cae625f5d
ciurad <damianciura2001@gmail.com>
2026-01-28 19:12:06 +0100

    configure in-memory database for UserServiceApplicationTests

--- user-service/src/test/java/pl/allegro/agh/budgetManagement/user/UserServiceApplicationTests.java
+++ user-service/src/test/java/pl/allegro/agh/budgetManagement/user/UserServiceApplicationTests.java
@@ -5,7 +5,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1",
+        "spring.datasource.driver-class-name=org.h2.Driver",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=",
+        "spring.flyway.enabled=false",
+        "spring.jpa.hibernate.ddl-auto=create-drop",
+        "app.security.enabled=false"
+})
 public class UserServiceApplicationTests {
 
     @Test
