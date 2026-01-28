3bc82b68051d660ee8d84b5d4abab9776bb4967d
ciurad <damianciura2001@gmail.com>
2026-01-28 22:57:47 +0100

    add OpenAPI configuration for Budget Service with JWT security scheme

--- budget-service/src/main/java/pl/allegro/agh/budgetManagement/budget/config/OpenApiConfig.java
+++ budget-service/src/main/java/pl/allegro/agh/budgetManagement/budget/config/OpenApiConfig.java
@@ -1,4 +1,32 @@
 package pl.allegro.agh.budgetManagement.budget.config;
 
-// OpenAPI configuration removed from budget-service to avoid runtime incompatibility with Spring Framework 7.
-// If you want to re-enable swagger, add the springdoc dependency back to build.gradle and restore this file.
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearer-jwt";
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                                .name("Authorization")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                        )
+                )
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .info(new Info()
+                        .title("Budget Service API")
+                        .version("v1")
+                        .description("OpenAPI documentation for the Budget Service"));
+    }
+}
--- user-service/src/main/java/pl/allegro/agh/budgetManagement/user/config/OpenApiConfig.java
+++ user-service/src/main/java/pl/allegro/agh/budgetManagement/user/config/OpenApiConfig.java
@@ -4,13 +4,26 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 
 @Configuration
 public class OpenApiConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearer-jwt";
         return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                                .name("Authorization")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                        )
+                )
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
                 .info(new Info()
                         .title("User Service API")
                         .version("v1")
